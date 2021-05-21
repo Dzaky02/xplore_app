5c8020c4457dc7dc44165729fb388acd62a36327
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-21 11:00:47 +0700

    Menyiapkan assets dan theme.

---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = Color(0XFFE5E5E5);
+Color primaryTextColor = Color(0XFF314728);
+Color secondTextColor = Color(0XFFA9B0A6);
+Color accentColor = Color(0XFFF9B650);
