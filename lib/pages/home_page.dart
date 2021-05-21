1ec66c1a93919ba4a3980ed8171d3629efea6bf4
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-21 18:32:52 +0700

    Finishing bottom sheet.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,109 +7,176 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 35,
-              ),
-              Text(
-                'Profile Picture',
-                style: TextStyle(
-                  color: primaryTextColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Image.asset(
-                'assets/images/primary.png',
-                height: 120,
-                width: 120,
-              ),
-              SizedBox(
-                height: 14,
-              ),
-              Text(
-                'Dzaky Brori',
-                style: TextStyle(
-                  color: primaryTextColor,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+                Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                    color: primaryTextColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              Text(
-                'Full-Stack Developer',
-                style: TextStyle(
-                  color: secondaryTextColor,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Wrap(
-                spacing: 38,
-                runSpacing: 40,
-                children: [
-                  Image.asset(
-                    'assets/images/item.png',
-                    width: 70,
+                Image.asset(
+                  'assets/images/primary.png',
+                  height: 120,
+                  width: 120,
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  'Dzaky Brori',
+                  style: TextStyle(
+                    color: primaryTextColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
                   ),
-                  Image.asset(
-                    'assets/images/item-1.png',
-                    width: 70,
+                ),
+                Text(
+                  'Full-Stack Developer',
+                  style: TextStyle(
+                    color: secondaryTextColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
                   ),
-                  Image.asset(
-                    'assets/images/item-2.png',
-                    width: 70,
-                  ),
-                  Image.asset(
-                    'assets/images/item-3.png',
-                    width: 70,
-                  ),
-                  Image.asset(
-                    'assets/images/item-4.png',
-                    width: 70,
-                  ),
-                  Image.asset(
-                    'assets/images/item-5.png',
-                    width: 70,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                width: 224,
-                height: 55,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Update Profile',
-                    style: TextStyle(
-                      color: primaryTextColor,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image.asset(
+                      'assets/images/item.png',
+                      width: 70,
+                    ),
+                    Image.asset(
+                      'assets/images/item-1.png',
+                      width: 70,
+                    ),
+                    Image.asset(
+                      'assets/images/item-2.png',
+                      width: 70,
+                    ),
+                    Image.asset(
+                      'assets/images/item-3.png',
+                      width: 70,
+                    ),
+                    Image.asset(
+                      'assets/images/item-4.png',
+                      width: 70,
+                    ),
+                    Image.asset(
+                      'assets/images/item-5.png',
+                      width: 70,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  width: 224,
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(25.0)),
+                        ),
+                        builder: (context) {
+                          return Container(
+                            height: 250,
+                            padding: EdgeInsets.symmetric(
+                              vertical: 40,
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Update Photo',
+                                  style: TextStyle(
+                                    color: primaryTextColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'You are only able to change\nthe picture profile once',
+                                  style: TextStyle(
+                                    color: secondaryTextColor,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Container(
+                                  width: 222,
+                                  height: 52,
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Continue',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: accentColor,
+                                      onSurface: Colors.black26,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(16),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        color: primaryTextColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onSurface: Colors.black26,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onSurface: Colors.black26,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-            ],
+                SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
           ),
         ),
       ),
